41c8019b0e121355c94e3dfe7b302495b69622a6
Infusca <Bluewater192@gmail.com>
2023-08-02 11:32:16 -0400

    new blog post, photography, images added

--- js/blog.js
+++ js/blog.js
@@ -1,5 +1,6 @@
 let arbolChili = $('#arbolchilihotsauce')[0];
 let buffalocalzone = $('#buffalochickencalzone')[0];
+let picklesNsauce = $('#picklesNsauce')[0];
 
 arbolChili.style.display = 'block';
 
@@ -9,45 +10,58 @@ arbolChili.style.display = 'block';
 
 
 function showBuffaloChicken(e){
-    arbolChili.style.display = 'none';
     buffalocalzone.style.display = 'block';
+    arbolChili.style.display = 'none';
+    picklesNsauce.style.display = 'none';
     targetID = e.target.href.split('#')[1];
     let blogTargetID = '#'+targetID;
     console.log(blogTargetID);
 }
 
 function showArbolChili(e){
-    buffalocalzone.style.display = 'none';
     arbolChili.style.display = 'block';
-    targetID = e.target.href.split('#')[1];
-    let blogTargetID = '#'+targetID;
-    console.log(blogTargetID);
-}
-
-function showStories(e){
     buffalocalzone.style.display = 'none';
-    arbolChili.style.display = 'none';
-    stories.style.display = 'block';
+    picklesNsauce.style.display = 'none';    
     targetID = e.target.href.split('#')[1];
     let blogTargetID = '#'+targetID;
     console.log(blogTargetID);
 }
 
-var attempt = 3;
-function validate(){
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
-    if (username == 'infusca' && password == 'admin84'){
-        window.location = 'blog/stories.html'; // log into stories page
-        return false;
-    } else {
-        attempt --;
-        alert('Insuccessful Login Attempt. You will now be eliminated.');
-        if (attempt == 0){
-            $('#username').disabled = true;
-            $('password').disabled = true;
-            return false;
-        }
-    }
-    console.log(attempt);
+function showPicklesNSauce(e){
+    picklesNsauce.style.display = 'block';
+    arbolChili.style.display = 'none';
+    buffalocalzone.style.display = 'none';
+    targetID = e.target.href.split('#')[1];
+    let blogTargetID = '#'+targetID;
+    console.log(blogTargetID);
 }
+
+
+
+// function showStories(e){
+//     buffalocalzone.style.display = 'none';
+//     arbolChili.style.display = 'none';
+//     stories.style.display = 'block';
+//     targetID = e.target.href.split('#')[1];
+//     let blogTargetID = '#'+targetID;
+//     console.log(blogTargetID);
+// }
+
+// var attempt = 3;
+// function validate(){
+//     var username = document.getElementById('username').value;
+//     var password = document.getElementById('password').value;
+//     if (username == 'infusca' && password == 'admin84'){
+//         window.location = 'blog/stories.html'; // log into stories page
+//         return false;
+//     } else {
+//         attempt --;
+//         alert('Insuccessful Login Attempt. You will now be eliminated.');
+//         if (attempt == 0){
+//             $('#username').disabled = true;
+//             $('password').disabled = true;
+//             return false;
+//         }
+//     }
+//     console.log(attempt);
+// }
