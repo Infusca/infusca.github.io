6ae27305ac57b333a7d37d276a6e76b7df8c3efc
Infusca <bluewater192@gmail.com>
2024-08-21 09:29:59 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+// const trails = document.querySelectorAll('.cursor-trail');
+
+// document.addEventListener('mousemove', (e) => {
+//     console.log("mouse moving")
+//     trails.forEach((trail, index) => {
+//         setTimeout(() => {
+//             trail.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
+//         }, index * 50); // Delay each trail slightly
+//     });
+// });
+
+soh = document.getElementById('soh')
+
+document.addEventListener('click', (e) => {
+    var target = e.target;
+    
+    
+    
+})
+
+soh.addEventListener('click', (e) => {
+    window.location.href = "index.html"
+})
